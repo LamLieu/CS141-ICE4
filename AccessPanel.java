8162f7041cd67067460865d67e4723830754727f
Lam Lieu <lieulam@hotmail.com>
2018-02-15 09:10:15 -0800

    Committing first few classes AccessPanel - driver program User - Superclass

---
+++ AccessPanel.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class AccessPanel {
+   public Scanner kb = new Scanner(System.in);
+      public static void main(String[] args) {
+         System.out.println("Welcome to your Access Panel.");
+   }
+}
