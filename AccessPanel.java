b210a76b07faf6f526c3c1d07371e26ae80501f5
Lam Lieu <lieulam@hotmail.com>
2018-02-15 21:06:53 -0800

    Fixed a small bug.

--- AccessPanel.java
+++ AccessPanel.java
@@ -22,13 +22,17 @@ public class AccessPanel {
       password = kb.nextLine();
 
       System.out.print("Are you an admin(1), vendor(2), or guest(3)?\n-->");
-      kb.nextLine();
       statusNumber = checkStatus();
+      do { //Checks if the admin has an admin code
+         if (haveAdminCode()) {
+            if (!checkAdminCode())
+               statusNumber = checkStatus();
+            else break;
+         }
+      } while (statusNumber == 1);
       switch (statusNumber) {
          case 1:
-            if (haveAdminCode())
-               if (checkAdminCode())
-                  user = new Admin(username, password);
+            if (haveAdminCode()) if (checkAdminCode()) user = new Admin(username, password);
          case 2:
             user = new Vendor(username, password);
          case 3:
@@ -43,12 +47,6 @@ public class AccessPanel {
          System.out.println("Error: Please enter 1, 2, or 3.");
          checkStatus();
       }
-      else if (num == 1) {
-         if (haveAdminCode())
-            if (checkAdminCode()) {
-
-            }
-      }
       else return num;
       return num;
    }
@@ -58,12 +56,15 @@ public class AccessPanel {
       String userYesOrNo = kb.nextLine();
       if (Character.toLowerCase(userYesOrNo.charAt(0)) == 'y') {
          return true;
-      } else if (Character.toLowerCase(userYesOrNo.charAt(0)) == 'n') {
-         return false;
-      } else {
-         System.out.println("Error: Please enter 'Y' or 'N'.");
-         haveAdminCode();
       }
+      else
+         if (Character.toLowerCase(userYesOrNo.charAt(0)) == 'n') {
+            return false;
+         }
+         else {
+            System.out.println("Error: Please enter 'Y' or 'N'.");
+            haveAdminCode();
+         }
       return false;
    }
 
@@ -72,12 +73,10 @@ public class AccessPanel {
       boolean check;
       System.out.print("Enter your admin code:\n-->");
       check = kb.nextLine().equals("abc123"); //abc123 is the admin code.
-      if (check)
-         return true;
+      if (check) return true;
       else {
          System.out.print("Would you like to reenter your admin code?(Y/N)\n-->");
-         if (Character.toLowerCase(kb.nextLine().charAt(0)) == 'y')
-            checkAdminCode();
+         if (Character.toLowerCase(kb.nextLine().charAt(0)) == 'y') checkAdminCode();
       }
       return false;
    }
