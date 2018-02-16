92934ae04f39fbcd8dae108dd416bc73c52dad64
Lam Lieu <lieulam@hotmail.com>
2018-02-15 20:54:31 -0800

    Committing and pushing whatever I have right now (may or may not be able to run)

--- User.java
+++ User.java
@@ -1,9 +1,31 @@
-public abstract class User {
-   private String status;
-   private String username;
-   private String adminCode;
+public abstract class User { // Explicitly states that the User class is the SuperClass
+   private String status, username, password;
+   private boolean admin = false;
 
    public User() {
+   }
 
+   public void setUsername(String username) {
+      this.username = username;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   public void setStatus(String status) {
+      this.status = status;
+   }
+
+   public void setAdmin() {
+      admin = true;
+   }
+
+   public String getUsername() {
+      return username;
+   }
+
+   public String getStatus() {
+      return status;
    }
 }
---
+++ Vendor.java
@@ -0,0 +1,7 @@
+public class Vendor extends User {
+   public Vendor(String username, String password) {
+      setUsername(username);
+      setPassword(password);
+      setStatus("Vendor");
+   }
+}
