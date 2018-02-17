e1effc848600c67191ab2bb79989b5ba52853051
beaceves <35122250+beaceves@users.noreply.github.com>
2018-02-17 15:35:44 -0800

    Added read/write to files

--- Admin.java
+++ Admin.java
@@ -3,17 +3,63 @@ import java.util.ArrayList;
 public class Admin extends User {
 
    private ArrayList<String> password = new ArrayList<String>("0000");
-   private ArrayList<String[]> Users = new ArrayList<String[]>();
+   private ArrayList<String> adminUsers = new ArrayList<String>("admin");
 
+   public Admin(){
+
+   }
+   
    public Admin(String username, String password) {
       setUsername(username);
       setPassword(password);
       setAdmin();
    }
 
-  public void addPassword(String pass){
-    password.add(pass);
-  }
+   public void addPassword(String pass){
+     password.add(pass);
+   }
+
+   public void readAdminFile(){
+     File file = new File("adminUsers.dat");
+     Scanner inputFile = new Scanner(file);
+     while (inputFile.hasNext()){
+     String str = inputFile.nextLine();
+     adminUsers.add(str);
+     }
+     inputFile.close();
+   }
+
+   public void removeAdmin(String user){
+     if(adminUsers.indexOf(user) < 0){
+       System.out.println("That user is not registered. Please enter the name of a registered user.");
+     }
+     else{
+       adminUsers.remove(adminUsers.indexOf(user));
+       System.out.println(user + " has been removed.");
+     }
+   }
+
+   public boolean checkPass(String pass){
+     boolean check = false;
+     for(int i = 0; i < adminUsers.size(); i++){
+       if(adminUsers.get(i).equals(pass)){
+        check = true;
+        break;
+      }
+     }
+     return check;
+   }
+
+   public void writeAdminsToFile(){
+     try{
+       PrintWriter outfile = new PrintWriter("adminUsers.dat");
+       for(int i = 0; i < adminUsers.size(); i++){
+         outfile.println(adminUsers.get(i));
+       }
+     } catch(FileNotFoundException e){
+       e.printStackTrace();
+     }
+   }
 
   /*
   public String toString() {
--- Login.java
+++ Login.java
@@ -15,14 +15,14 @@ public class Login {
 
       System.out.print("Are you an admin(1), vendor(2), or guest(3)?\n-->");
       statusNumber = checkStatus();
-      do { //Checks if the admin has an admin code
+      /*while (statusNumber == 1) { //Checks if the admin has an admin code
          if (haveAdminCode()) {
             if (!checkAdminCode())
                statusNumber = checkStatus();
             else
                break;
          }
-      } while (statusNumber == 1);
+      }*/
 
       switch (statusNumber) { //creates user profile
          case 1:
@@ -37,44 +37,49 @@ public class Login {
    }
 
    public int checkStatus() { //gets status of user
-      int num = kb.nextInt();
-      kb.nextLine();
-      if (num != 1 || num != 2 || num != 3) {
-         System.out.println("Error: Please enter 1, 2, or 3.");
-         System.out.print("Are you an admin(1), vendor(2), or guest(3)?\n-->");
-         checkStatus();
+      while(true){
+        int num = kb.nextInt();
+        kb.nextLine();
+        if (num != 1 || num != 2 || num != 3) {
+           System.out.println("Error: Please enter 1, 2, or 3.");
+           System.out.print("Are you an admin(1), vendor(2), or guest(3)?\n-->");
+        }
+        else
+          break;
       }
-      else
-         return num;
       return num;
    }
 
    public boolean haveAdminCode() { //asks user if he/she has an admin code
       System.out.println("Do you have an admin code?(Y/N)\n-->");
-      String userYesOrNo = kb.nextLine();
-      if (Character.toLowerCase(userYesOrNo.charAt(0)) == 'y') {
-         return true;
-      }
-      else if (Character.toLowerCase(userYesOrNo.charAt(0)) == 'n') {
-         return false;
-      }
-      else {
-         System.out.println("Error: Please enter 'Y' or 'N'.");
-         haveAdminCode();
+      while(true){
+        String userYesOrNo = kb.nextLine();
+        if (Character.toLowerCase(userYesOrNo.charAt(0)) == 'y') {
+           return true;
+        }
+        else if (Character.toLowerCase(userYesOrNo.charAt(0)) == 'n') {
+           return false;
+        }
+        else {
+           System.out.println("Error: Please enter 'Y' or 'N'.");
+        }
       }
       return false;
    }
 
    public boolean checkAdminCode() { //checks user's admin code
       boolean check;
+      Admin admin = new Admin();
       System.out.print("Enter your admin code:\n-->");
-      check = kb.nextLine().equals("abc123"); //abc123 is the admin code.
-      if (check)
-         return true;
-      else {
-         System.out.print("Would you like to reenter your admin code?(Y/N)\n-->");
-         if (Character.toLowerCase(kb.nextLine().charAt(0)) == 'y')
-            checkAdminCode();
+      while(true){
+        check = admin.checkPass(); //uses method from admin class
+        if (check)
+           return true;
+        else {
+           System.out.print("Would you like to reenter your admin code?(Y/N)\n-->");
+           if (Character.toLowerCase(kb.nextLine().charAt(0)) == 'y')
+              checkAdminCode();
+        }
       }
       return false;
    }
