bde1bd1c353e8a1b803ead405c3a6b1df6ae6e79
Lam Lieu <lieulam@hotmail.com>
2018-02-19 10:06:24 -0800

    Added permissions for Vendor and Guest. Fixed a few bugs.

--- Permissions.java
+++ Permissions.java
@@ -25,41 +25,68 @@ public class Permissions {
       int choice;
       String name;
       Scanner kb = new Scanner(System.in);
-      if (user.isAdmin()) { //Admin options
-         System.out.printf("Hello %s, What do you want to do:\n(1) Create a Vendor or Guest Account.\n"
-                 + "(2) Set new ticket prices.\n(3) Change Password.\n(4) Exit Program.\n-->", user.getUsername());
+      char userAnswer;
+
+      //Admin Options
+      if (user.isAdmin()) {
+         System.out.printf("Hello %s, What do you want to do:\n(1) Create a Vendor or Guest Account.\n" + "(2) Set new ticket prices.\n(3) Set new space price.\n" + "(4) Change Password.\n(5) Exit Program.\n-->", user.getUsername());
          choice = kb.nextInt();
          do {
             switch (choice) {
                case 1:
-                  System.out.print("Which type of account would you like to create? \n(1)Create a Vendor \n(2)Guest Account.\n");
+                  System.out.print("Which type of account would you like to create? \n(1) Create a Vendor\n" + "(2) Guest Account.\n-->");
                   choice = kb.nextInt();
                   do {
-                    switch (choice) {
-                      case 1:
-                        System.out.print("What is the guest name?");
-                        name = kb.nextLine();
-                        guest = new Guest(name, "abc123");
-                      case 2:
-                        System.out.print("What is the vendor name?");
-                        name = kb.nextLine();
-                        vendor = new Vendor(name, "abc123");
-                      default:
-                         System.out.println("Error: Please enter 1 or 2");
-                         choice = kb.nextInt();
-                    }
-                  } while(choice != 1 && choice != 2);
+                     switch (choice) {
+                        case 1:
+                           System.out.print("What is the guest name?\n-->");
+                           name = kb.nextLine();
+                           guest = new Guest(name, "abc123");
+                           break;
+                        case 2:
+                           System.out.print("What is the vendor name?\n-->");
+                           name = kb.nextLine();
+                           vendor = new Vendor(name, "abc123");
+                           break;
+                        default:
+                           System.out.println("Error: Invalid input.");
+                           choice = kb.nextInt();
+                     }
+                  } while (choice != 1 && choice != 2);
                   break;
                case 2:
-                  System.out.printf("The price of a ticket is currently $%.2f.\n", user.getPrice());
+                  System.out.printf("The price of a ticket is currently $%.2f.\n", user.getTicketPrice());
                   System.out.print("Would you like to set a new ticket price?(Y/N)\n-->");
-                  if (Character.toLowerCase(kb.nextLine().charAt(0)) == 'y') {
+                  userAnswer = Character.toLowerCase(kb.next().charAt(0));
+                  if (userAnswer == 'y') {
                      System.out.print("Enter the new ticket price.\n-->");
                      user.setPrice(kb.nextInt());
-                     System.out.printf("The new ticket price is %.2f.", user.getPrice());
+                     System.out.printf("The new ticket price is %.2f.", user.getTicketPrice());
+                  }
+                  else if (userAnswer == 'n') {
+                     System.out.println("The price of the space remains the same.");
+                  }
+                  else {
+                     System.out.println("Error: Invalid response.");
                   }
                   break;
                case 3:
+                  System.out.printf("The price of a space is currently $%.2f.\n", user.getSpacePrice());
+                  System.out.print("Would you like to set a new space price?(Y/N)\n-->");
+                  userAnswer = Character.toLowerCase(kb.next().charAt(0));
+                  if (userAnswer == 'y') {
+                     System.out.print("Enter the new space price.\n-->");
+                     user.setSpacePrice(kb.nextInt());
+                     System.out.printf("The new ticket price is %.2f.", user.getSpacePrice());
+                  }
+                  else if (userAnswer == 'n') {
+                     System.out.println("The price of the space remains the same.");
+                  }
+                  else {
+                     System.out.println("Error: Invalid input.");
+                  }
+                  break;
+               case 4:
                   String currentPasswordInput, newPasswordInput;
                   do {
                      System.out.print("Enter your current password. (or -1 to go back to menu)\n-->");
@@ -84,11 +111,12 @@ public class Permissions {
                         break;
                      }
                      else {
-                        System.out.println("Error: Wrong password. Please reenter your password or type '-1' to exit");
+                        System.out.println("Error: Wrong password.\n" +
+                              "Please reenter your password or type '-1' to return to menu.");
                      }
                   } while (!currentPasswordInput.equals(user.getPassword()) || !currentPasswordInput.equals("-1"));
                   break;
-               case 4:
+               case 5:
                   System.out.println("Thank you for using the access panel.");
                   System.exit(0);
                default:
@@ -96,15 +124,68 @@ public class Permissions {
                   choice = kb.nextInt();
             }
          } while (choice < 1 || choice > 4);
+         System.out.println("Returning back to menu...");
          chooseOption();
       }
-      else if (user.isVendor()) { //Need to add permissions
-         System.out.printf("Hello %s, what do you want to do:\n(1) ", user.getUsername());
-         //chooseOption();
+
+      //Vendor Options
+      else if (user.isVendor()) {
+         System.out.printf("Hello %s, what do you want to do:\n(1) Purchase space.\n" + "(2) Exit Program.", user.getUsername());
+         choice = kb.nextInt();
+         do {
+            switch (choice) {
+               case 1:
+                  System.out.printf("The price of a spot is $%.2f.", user.getSpacePrice());
+                  System.out.print("Would you like to set a new space price?(Y/N)\n-->");
+                  userAnswer = Character.toLowerCase(kb.next().charAt(0));
+                  if (userAnswer == 'y') {
+                     System.out.println("Thank you for purchasing a ticket.");
+                  }
+                  else if (userAnswer == 'n') {
+                     break;
+               }
+                  else {
+                     System.out.println("Error: Invalid input.");
+                  }
+                  chooseOption();
+                  break;
+               case 2:
+                  System.out.println("Thank you for using the access panel.");
+                  System.exit(0);
+            }
+         } while (choice < 1 || choice > 4);
+         System.out.println("Returning back to menu...");
+         chooseOption();
       }
+
+      //Guest Options
       else if (user.isGuest()) { //Need to add permissions
-         System.out.printf("Hello %s, what do you want to do:\n(1) ", user.getUsername());
-         //chooseOption();
+         System.out.printf("Hello %s, what do you want to do:\n(1) Purchase ticket.\n" + "(2) Exit Program.", user.getUsername());
+         choice = kb.nextInt();
+         do {
+            switch (choice) {
+               case 1:
+                  System.out.printf("The ticket price is $%.2f.", user.getTicketPrice());
+                  System.out.print("Would you like to set a new space price?(Y/N)\n-->");
+                  userAnswer = Character.toLowerCase(kb.nextLine().charAt(0));
+                  if (userAnswer == 'y') {
+                     System.out.println("Thank you for purchasing a ticket.");
+                     System.out.println("Returning back to menu...");
+                  }
+                  else if (userAnswer == 'n'){
+                     break;
+                  }
+                  else {
+                     System.out.println("Error: Invalid input.");
+                  }
+                  break;
+               case 2:
+                  System.out.println("Thank you for using the access panel.");
+                  System.exit(0);
+            }
+         } while (choice < 1 || choice > 4);
+         System.out.println("Returning back to menu...");
+         chooseOption();
       }
    }
 }
